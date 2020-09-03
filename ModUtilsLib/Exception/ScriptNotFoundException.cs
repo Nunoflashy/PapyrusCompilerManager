b432de024114b0396f1a4601477b3b80797337fc
Nunoflashy <nunoratchet@gmail.com>
2020-09-03 18:23:13 +0100

    Add files via upload

---
+++ ModUtilsLib/Exception/ScriptNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModUtilsLib.Exception
+{
+    /// <summary>
+    /// The exception that is thrown when an attempt to access a script from a specified mod does not exist.
+    /// </summary>
+    public class ScriptNotFoundException : System.Exception
+    {
+        public ScriptNotFoundException() : base()
+        {
+
+        }
+
+        public ScriptNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ ModUtilsLib/Exception/ScriptsDirectoryNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModUtilsLib.Exception
+{
+    /// <summary>
+    /// The exception that is thrown when a mod does not contain a scripts directory.
+    /// </summary>
+    public class ScriptsDirectoryNotFoundException : System.Exception
+    {
+        public ScriptsDirectoryNotFoundException() : base()
+        {
+
+        }
+
+        public ScriptsDirectoryNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
